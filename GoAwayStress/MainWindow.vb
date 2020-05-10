e61e76c8a6cf1c85e1134f55299e587952688ebc
Matto58 <mattothepou@gmail.com>
2020-05-10 18:07:39 +0200

    update hehe

--- GoAwayStress/MainWindow.vb
+++ GoAwayStress/MainWindow.vb
@@ -1,3 +1,13 @@
 ﻿Class MainWindow
+    Private Sub ListBox_SelectionChanged(sender As Object, e As SelectionChangedEventArgs)
 
+    End Sub
+
+    Private Sub Slider_ValueChanged(sender As Object, e As RoutedPropertyChangedEventArgs(Of Double))
+
+    End Sub
+
+    Private Sub RadioButton_Checked(sender As Object, e As RoutedEventArgs)
+
+    End Sub
 End Class
